be6501f175932d65766dca92bffcf87e5138f1be
Gabriel.s <gabrielsantosayres@gmail.com>
2023-08-30 15:12:32 -0300

    Acenda e apague a luz com jS

---
+++ assets/js/luz.js
@@ -0,0 +1,7 @@
+function acender() {
+    document.querySelector('#lâmpada').src = 'assets/img/pic_bulbon.gif';
+}
+
+function apagar() {
+    document.querySelector('#lâmpada').src = 'assets/img/pic_bulboff.gif';
+}
